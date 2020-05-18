b5d545ef5e3df50cd89f6314a8d3b96eabd1980d
Cristiano Morgante <cristianomorgante@hotmail.com>
2020-05-17 23:49:27 -0300

    Modify icon change theme

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from "react"
-import getThemeColor from "../../utils/getThemeColor"
-
-import { Home } from "styled-icons/boxicons-solid/Home"
-import { SearchAlt2 as Search } from "styled-icons/boxicons-regular/SearchAlt2"
 // import { UpArrowAlt as Arrow } from "styled-icons/boxicons-regular/UpArrowAlt"
-import { LightbulbOutline as Light } from "styled-icons/material/LightbulbOutline"
+import { LightBulb as Light } from "@styled-icons/octicons/LightBulb"
+import React, { useEffect, useState } from "react"
+import { SearchAlt2 as Search } from "styled-icons/boxicons-regular/SearchAlt2"
 import { Grid } from "styled-icons/boxicons-solid/Grid"
+import { Home } from "styled-icons/boxicons-solid/Home"
 import { ThList as List } from "styled-icons/typicons/ThList"
-
+import getThemeColor from "../../utils/getThemeColor"
 import * as S from "./styled"
+
+// import { UpArrowAlt as Arrow } from "styled-icons/boxicons-regular/UpArrowAlt"
 
 const MenuBar = () => {
   const [theme, setTheme] = useState(null)
